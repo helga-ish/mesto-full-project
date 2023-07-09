23ead1fd780cfaee98bd7e8cf96679992e35cd97
Helga Ish <belfegor.nomnom@gmail.com>
2023-07-09 22:23:41 +0800

    feat: rmv api

--- backend/app.js
+++ backend/app.js
@@ -30,14 +30,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(requestLogger);
 
-app.post('/api/signin', celebrate({
+app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
 }), login);
 
-app.post('/api/signup', celebrate({
+app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
