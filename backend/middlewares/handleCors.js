1f4c4015c6c8a34950ed2cd60f8050255e1d6d00
Helga Ish <belfegor.nomnom@gmail.com>
2023-07-10 00:36:24 +0800

    fix: cors problem

--- backend/middlewares/handleCors.js
+++ backend/middlewares/handleCors.js
@@ -1,7 +1,5 @@
 // const allowedCors = [
-//   'https://praktikum.tk',
-//   'http://praktikum.tk',
-//   'localhost:3000',
+//   'https://mesto.place.nomoredomains.work',
 // ];
 
 // app.use(function(req, res, next) {
@@ -9,5 +7,11 @@
 //   if (allowedCors.includes(origin)) {
 //     res.header('Access-Control-Allow-Origin', origin);
 //   }
+//   const { method } = req;
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//   }
+
 //   next();
 // });
