0ecd801a3eaf950b28b77089d1217aeb3c1c989c
Helga Ish <belfegor.nomnom@gmail.com>
2023-07-10 19:57:15 +0800

    hotfix mongo

--- backend/app.js
+++ backend/app.js
@@ -28,11 +28,7 @@ mongoose.connect(
   'mongodb://localhost:27017/mestodb',
   {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useFindAndModify: false,
-  },
-  () => {
-    console.log('mongodb is connected');
   },
 );
 
