8b29e8881fc11f5b7bed5624fdfb1788c201d339
Helga Ish <belfegor.nomnom@gmail.com>
2023-07-11 20:12:25 +0800

    feat: env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 const UnauthorizedError = require('../components/UnauthorizedError');
 
 module.exports = (req, res, next) => {
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',);
   } catch (err) {
     return next(new UnauthorizedError());
   }
