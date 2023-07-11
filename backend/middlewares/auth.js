43409086b5b6ce968842b5a66f572460ace32bab
Helga Ish <belfegor.nomnom@gmail.com>
2023-07-11 20:13:06 +0800

    fix: env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new UnauthorizedError());
   }
