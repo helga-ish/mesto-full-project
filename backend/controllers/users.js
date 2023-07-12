530b608f840de8cc23f11bc28e61626e5cd97afa
Helga Ish <belfegor.nomnom@gmail.com>
2023-07-12 16:32:16 +0800

    fix: rmv User.findOne()

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -10,21 +10,13 @@ const login = (req, res, next) => {
   const { email, password } = req.body;
 
   return User.findUserByCredentials(email, password)
-    .then(() => {
-      User.findOne(
-        {
-          $or: [{ email: req.body.email }, { password: req.body.password }],
-        },
-      )
-        .then((user) => {
-          const token = jwt.sign(
-            { _id: user._id },
-            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-            { expiresIn: '7d' },
-          );
-          res.send({ token });
-        })
-        .catch(next);
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
+      res.send({ token });
     })
     .catch(next);
 };
