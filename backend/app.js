ac9eecf7fc7e43264a1502d184fea0369cbd1a85
Helga Ish <belfegor.nomnom@gmail.com>
2023-07-12 16:17:49 +0800

    fix: rmv unused path

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 const bodyParser = require('body-parser');
 const {
   celebrate,
