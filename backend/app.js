a2a0379d2687196defc4a3da84944d361743fafb
Helga Ish <belfegor.nomnom@gmail.com>
2023-07-12 19:56:19 +0800

    refact: delete bodyParser

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const {
   celebrate,
   Joi,
@@ -22,8 +21,8 @@ const NotFoundError = require('./components/NotFoundError');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const uri = 'mongodb://0.0.0.0:27017/mongodb';
 
